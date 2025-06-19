14736ab5408d75ac0f7da4f33ea96312dde6c697
unknown <bappadityasinha.92@gmail.com>
2025-06-19 14:57:22 +0530

    adding some code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import React from 'react'
 import Home from './pages/home'
+import Cart from './pages/cart'
 
 const App = () => {
   return (
@@ -8,7 +8,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>} />
         <Route path="/search" element={<search/>}/>
-        <Route path="/cart" element={<cart/>}>
+        <Route path="/cart" element={<Cart/>}>
       </Routes>
     </Router>
   )
