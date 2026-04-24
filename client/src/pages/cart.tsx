75247e5b53f58573d33f58cfbf8f975dadce0a0f
bappaditya92 <bappadityasinha.92@gmail.com>
2026-04-24 17:44:57 +0530

    remove client code

--- client/src/pages/cart.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Cart = () => {
-  return (
-    <div>This is from Cart page</div>
-  )
-}
-
-export default Cart;
--- client/src/pages/search.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Search = () => {
-  return (
-    <div>This is the Search Page</div>
-  )
-}
-
-export default Search;
